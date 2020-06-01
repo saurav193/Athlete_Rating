dc510f623d63cc190bc4a9c43f7f1530a24dc5da
saurav99 <saurav13iitr@gmail.com>
2020-05-31 17:12:02 -0700

    updated approach for modelling

--- src/data_download_and_compile.py
+++ src/data_download_and_compile.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import dropbox
 import os
+import sys
 
 dbx = dropbox.Dropbox(os.environ['DROPBOX_API'])
 dbx.users_get_current_account()
@@ -28,9 +29,9 @@ filtered_data['Athlete_name'] =  final_df['Athlete/Comp'].str.extract(r"(.*-....
 
 filtered_data = filtered_data.reset_index(drop = True)
 
-#Updating old scores in same scale as new metric for scores before 2014
-filtered_data['Metric'] = [round(filtered_data.loc[i,'Score']/30*100, 2) if filtered_data.loc[i, 'Metric'] != filtered_data.loc[i,'Score'] \
-        else filtered_data.loc[i, 'Metric'] for i in range(len(filtered_data))]
+#Updating old scores in same scale as new score metric for scores before 2014
+filtered_data['Metric'] = [round(filtered_data.loc[i,'Score']/30*100, 2) if filtered_data.loc[i, 'Metric'] != filtered_data.loc[i,'Score']\
+                            else filtered_data.loc[i, 'Metric'] for i in range(len(filtered_data))]
 
 # Dropping the scores column
 filtered_data = filtered_data.drop(columns = ['Score'])
@@ -42,18 +43,15 @@ filtered_data = filtered_data[filtered_data['Round'].str.lower().str.extract(r'.
 tier1_df = filtered_data[filtered_data['Season_Comp'].str.lower().str.extract(r'.*(world).*', expand=False).notnull() |\
                  filtered_data['Season_Comp'].str.lower().str.extract(r'.*(olympics).*', expand=False).notnull()]
 
-print("Number of rows in tier 1 = ", tier1_df.shape[0])
-
 tier2_df = filtered_data[filtered_data['Season_Comp'].str.lower().str.extract(r'.*(north american).*', expand=False).notnull()]
-print("Number of rows in tier 2 = ", tier2_df.shape[0])
 
 tier3_df = filtered_data[filtered_data['Season_Comp'].str.lower().str.extract(r'.*(canad).*', expand=False).notnull()]
-print("Number of rows in tier 3 = ", tier3_df.shape[0])
 
 if tier1_df.shape[0]+tier2_df.shape[0]+tier3_df.shape[0] == filtered_data.shape[0]:
-    print("Total # of rows match")
+    print("Total # of rows match in each tier match with full data")
 else:
-    print("Rows mismatch")
+    print("# Rows mismatch for 3 tiers.")
+    sys.exit("Check the competition tier division logic")
 
 # changing order and name of columns
 tier1_df = tier1_df.loc[:, ['Athlete_Comp', 'Athlete_name', 'Gender', 'age_yrs', 'Nation', 'Judge_1_Score', 'Judge_2_Score', 
@@ -92,4 +90,33 @@ tier1_df_W.to_csv("../data/tier_1_comp_data_women.csv", index = False)
 tier2_df_W.to_csv("../data/tier_2_comp_data_women.csv", index = False)
 tier3_df_W.to_csv("../data/tier_3_comp_data_women.csv", index = False)
 
-print("data compilation successful!")
+print("data downloaded and stored locally.. uploading to dropbox")
+
+#uploading the processed files to Dropbox
+files = ['tier_1_comp_data_men.csv', 'tier_2_comp_data_men.csv', 'tier_3_comp_data_men.csv',
+         'tier_1_comp_data_women.csv', 'tier_2_comp_data_women.csv', 'tier_3_comp_data_women.csv']
+
+local_path = "../data/"
+dest_path= "/Saurav/data/compiled_data/"
+
+for file in files:
+    filepath = local_path+file
+    
+    with open(filepath, "rb") as f:
+       # we want to overwite any previous version of the file
+        try:
+            dbx.files_upload(f.read(), dest_path+file, mode=dropbox.files.WriteMode("overwrite"))
+            print(file+" uploaded")
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().error.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+
+print("Files uploaded to "+dest_path)
